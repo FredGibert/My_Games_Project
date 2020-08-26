2691fe35ae780fc5ca67706befea4e86db55353d
julien evano <julien.evano@audencia.com>
2020-08-26 11:36:20 +0200

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,21 @@ Rails.application.routes.draw do
 
   resource :profile, only: [:edit, :update]
 
+  resources :user_games, only: [:create]
 
+  resources :users, only: [:index, :show]
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :friend_requests, only: [:create]
+
+  resources :friends, only: [:index]
+
+  resources :events, only: [:new, :create]
+
+  resources :participations do
+    member do
+      patch :accept
+      patch :decline
+    end
+  end
+
 end
