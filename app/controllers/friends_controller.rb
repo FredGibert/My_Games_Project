961acac85989d3b7cdf8d43117c2f874188580e8
julien evano <julien.evano@audencia.com>
2020-08-29 01:18:43 +0200

    hello

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,5 +1,8 @@
 class FriendsController < ApplicationController
 
   def index
+    @friends = Friend.where(
+      "user1_id = :current_user_id OR user2_id = :current_user_id", current_user_id: current_user
+    ).order(created_at: :desc)
   end
 end
