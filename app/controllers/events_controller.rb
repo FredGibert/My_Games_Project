a998e0c316f298b1a26dde4de05ccaa5fbad5290
Julien Victor <victor.julien84@gmail.com>
2020-08-31 16:10:05 +0200

    event checkbox create

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,9 +13,15 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.new(event_params)
+    @event.user_id = current_user.id
+
     if @event.save
-      redirect_to post_path(@event)
+      invite_participants_to_new_event
+      post_new_event_to_friends_feed
+
+      redirect_to events_path
     else
+      @user_games = UserGame.includes(:game).where(user: current_user).limit(3)
       render :new
     end
   end
@@ -23,6 +29,19 @@ class EventsController < ApplicationController
   private
 
   def event_params
-    params.require(:event).permit(:description)
+    params.require(:event).permit(:description, :game_id, :start_at, :end_at)
+  end
+
+  def post_new_event_to_friends_feed
+    # Creation Feeds activities
+    @all_friends = current_user.friends
+    @all_friends.each do |friend|
+      receiver = friend.friend_of(current_user)
+      FeedActivity.create(content: @event, author: current_user, receiver: receiver)
+    end
+  end
+
+  def invite_participants_to_new_event
+    params[:participant_ids].each { |participant_id| @event.participations.create(user_id: participant_id) }
   end
 end
