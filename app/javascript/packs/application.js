409cc9eb396354f605ed0afd9a4dfd4e5f59d389
julien evano <julien.evano@audencia.com>
2020-09-01 14:43:38 +0200

    hello

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,3 +34,16 @@ document.addEventListener('turbolinks:load', () => {
   // initSelect2();
   loadCarousel();
 });
+
+
+// pour chaque game_id
+const games = document.querySelectorAll(".card-game");
+const game_selected = document.querySelector(".game-selected");
+
+games.forEach( game => {
+  game.addEventListener("click", (event) => {
+    game_selected.innerHTML = event.currentTarget.innerHTML;
+  });
+})
+
+
