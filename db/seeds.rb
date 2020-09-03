98046c19786471e6962f085893223fa6fa8bf0a6
julien evano <julien.evano@audencia.com>
2020-09-03 12:46:37 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,100 +40,118 @@ end
 
 # Users
 puts "J'ai tout détruit mon pote"
-julienv = User.create!(first_name: 'julien', last_name: 'victor', email: 'jvictor@exemple.com', password: 'secret', steam_username: "KillBill92", xboxlive_username: "KillBilly")
+julienv = User.new(first_name: 'julien', last_name: 'victor', email: 'jvictor@exemple.com', password: 'secret', steam_username: "KillBill92", xboxlive_username: "KillBilly")
 julienv_image = File.open(Rails.root.join("db/fixtures/users/julienv.jpeg"))
 julienv.photo.attach(io: julienv_image, filename: "julienv.jpeg")
+julienv.save!
 
-edouard = User.create!(first_name: 'edouard', last_name: 'goisbault', email: 'edouard@exemple.com', password: 'secret', steam_username: "eucalyptuus", psn_username: "eucalyptus340")
+edouard = User.new(first_name: 'edouard', last_name: 'goisbault', email: 'edouard@exemple.com', password: 'secret', steam_username: "eucalyptuus", psn_username: "eucalyptus340")
 edouard_image = File.open(Rails.root.join("db/fixtures/users/edouard.jpeg"))
 edouard.photo.attach(io: edouard_image, filename: "edouard.jpeg")
+edouard.save!
 
-fred = User.create!(first_name: 'fred', last_name: 'gibert', email: 'fred@exemple.com', password: 'secret', psn_username: "Fred Locks")
+fred = User.new(first_name: 'fred', last_name: 'gibert', email: 'fred@exemple.com', password: 'secret', psn_username: "Fred Locks")
 fred_image = File.open(Rails.root.join("db/fixtures/users/fred.png"))
 fred.photo.attach(io: fred_image, filename: "fred.png")
 puts "0x450074 hacked by MACosX"
+fred.save!
 
-juliene = User.create!(first_name: 'julien', last_name: 'evano', email: 'julien@exemple.com', password: 'secret', bio: "Disponible tous les soirs entre 21h et 23h", steam_username: "Juliano")
+juliene = User.new(first_name: 'julien', last_name: 'evano', email: 'julien@exemple.com', password: 'secret', bio: "Disponible tous les soirs entre 21h et 23h", steam_username: "Juliano")
 juliene_image = File.open(Rails.root.join("db/fixtures/users/juliene.jpeg"))
 juliene.photo.attach(io: juliene_image, filename: "juliene.jpeg")
+juliene.save!
 
 
 # Personnages
-alice = User.create!(first_name: 'Alice', last_name: 'Grandchamp', email: 'alice@exemple.com', password: 'secret', bio: "Je joue sans prétention à plusieurs FPS dont Counter Strike", steam_username: "alice")
+alice = User.new(first_name: 'Alice', last_name: 'Grandchamp', email: 'alice@exemple.com', password: 'secret', bio: "Je joue sans prétention à plusieurs FPS dont Counter Strike", steam_username: "alice")
 alice_image = File.open(Rails.root.join("db/fixtures/users/alice.png"))
 alice.photo.attach(io: alice_image, filename: "alice.png")
+alice.save!
 
-bob = User.create!(first_name: 'Bob', last_name: 'Lennon', email: 'bob@exemple.com', password: 'secret', bio: "Hello les gamers, c'est Bob. N'hésitez pas à m'inviter pour jouer à Fortnite ou Call of !", steam_username: "bob", psn_username: "BobbyPlay")
+bob = User.new(first_name: 'Bob', last_name: 'Lennon', email: 'bob@exemple.com', password: 'secret', bio: "Hello les gamers, c'est Bob. N'hésitez pas à m'inviter pour jouer à Fortnite ou Call of !", steam_username: "bob", psn_username: "BobbyPlay")
 bob_image = File.open(Rails.root.join("db/fixtures/users/bob.png"))
 bob.photo.attach(io: bob_image, filename: "bob.png")
+bob.save!
 
 puts "Comment ça va?"
 # Games
 
-fortnite = Game.create!(name: "Fortnite", description: "Battle Royale : 100 joueurs se battent dans des espaces de plus en plus petits pour être le dernier en vie.", on_psn: true, on_xboxlive: true)
+fortnite = Game.new(name: "Fortnite", description: "Battle Royale : 100 joueurs se battent dans des espaces de plus en plus petits pour être le dernier en vie.", on_psn: true, on_xboxlive: true)
 fortnite_image = File.open(Rails.root.join("db/fixtures/games/fortnite.jpg"))
 fortnite.photo.attach(io: fortnite_image, filename: "fortnite.jpg")
+fortnite.save!
 
-callofduty = Game.create!(name: "Call of Duty: Warzone", description: "Combat multijoueur entre 150 joueurs dans la ville de Verdansk.", on_steam: true, on_psn: true, on_xboxlive: true)
+callofduty = Game.new(name: "Call of Duty: Warzone", description: "Combat multijoueur entre 150 joueurs dans la ville de Verdansk.", on_steam: true, on_psn: true, on_xboxlive: true)
 warzone_image = File.open(Rails.root.join("db/fixtures/games/warzone.jpeg"))
 callofduty.photo.attach(io: warzone_image, filename: "warzone.jpeg")
+callofduty.save!
 puts "3..."
-overwatch = Game.create!(name: "Overwatch", description: "Coopération entre différentes classes représentées par différents personnages ayant chacun leurs capacités et particularités", on_psn: true, on_xboxlive: true)
+overwatch = Game.new(name: "Overwatch", description: "Coopération entre différentes classes représentées par différents personnages ayant chacun leurs capacités et particularités", on_psn: true, on_xboxlive: true)
 overwatch_image = File.open(Rails.root.join("db/fixtures/games/overwatch.jpg"))
 overwatch.photo.attach(io: overwatch_image, filename: "overwatch.jpg")
+overwatch.save!
 
-animalcross = Game.create!(name: "Animal Crossing: New Horizons", description: "Simulation de vie dans laquelle le joueur emménage dans un village habité par des animaux anthropomorphes.", on_steam: true)
+animalcross = Game.new(name: "Animal Crossing: New Horizons", description: "Simulation de vie dans laquelle le joueur emménage dans un village habité par des animaux anthropomorphes.", on_steam: true)
 animal_crossing_image = File.open(Rails.root.join("db/fixtures/games/animal_crossing.jpg"))
 animalcross.photo.attach(io: animal_crossing_image, filename: "animal_crossing.jpg")
+animalcross.save!
 puts "2..."
 
-gta = Game.create!(name: "Grand Theft Auto V", description: "Incarnation d'un criminel qui évolue grâce aux missions qui lui sont données.", on_psn: true, on_xboxlive: true)
+gta = Game.new(name: "Grand Theft Auto V", description: "Incarnation d'un criminel qui évolue grâce aux missions qui lui sont données.", on_psn: true, on_xboxlive: true)
 gta5_image = File.open(Rails.root.join("db/fixtures/games/gta5.jpg"))
 gta.photo.attach(io: gta5_image, filename: "gta5.jpg")
+gta.save!
 
-rocketleague = Game.create!(name: "Rocket League", description: "Deux équipes, composée de un à quatre joueurs conduisant des voitures, doivent frapper un ballon afin de marquer dans le but adverse.", on_psn: true, on_xboxlive: true)
+rocketleague = Game.new(name: "Rocket League", description: "Deux équipes, composée de un à quatre joueurs conduisant des voitures, doivent frapper un ballon afin de marquer dans le but adverse.", on_psn: true, on_xboxlive: true)
 rocketleague_image = File.open(Rails.root.join("db/fixtures/games/rocketleague.jpg"))
 rocketleague.photo.attach(io: rocketleague_image, filename: "rocketleague.jpg")
+rocketleague.save!
 puts "1..."
-hearthstone = Game.create!(name: "Hearthstone", description: " Bataille de cartes contre un joueur adverse sur un plateau de jeu virtuel.", on_psn: true, on_xboxlive: true)
+hearthstone = Game.new(name: "Hearthstone", description: " Bataille de cartes contre un joueur adverse sur un plateau de jeu virtuel.", on_psn: true, on_xboxlive: true)
 hearthstone_image = File.open(Rails.root.join("db/fixtures/games/hearthstone.jpeg"))
 hearthstone.photo.attach(io: hearthstone_image, filename: "hearthstone.jpeg")
 
-pubg = Game.create!(name: "PlayerUnknown's Battlegrounds", description: "Jeu de tir à la première personne et à la troisième personne de type joueur contre joueur dans lequel une centaine de joueurs se battent dans un battle royale.", on_psn: true, on_xboxlive: true)
+pubg = Game.new(name: "PlayerUnknown's Battlegrounds", description: "Jeu de tir à la première personne et à la troisième personne de type joueur contre joueur dans lequel une centaine de joueurs se battent dans un battle royale.", on_psn: true, on_xboxlive: true)
 pubg_image = File.open(Rails.root.join("db/fixtures/games/pubg.jpg"))
 pubg.photo.attach(io: pubg_image, filename: "pubg.jpg")
+pubg.save!
 puts "Oups ! C'était une feinte ;)"
 
-minecraft = Game.create!(name: "Minecraft", description: "Jeu vidéo de type « bac à sable » (construction complètement libre).", on_psn: true, on_xboxlive: true)
+minecraft = Game.new(name: "Minecraft", description: "Jeu vidéo de type « bac à sable » (construction complètement libre).", on_psn: true, on_xboxlive: true)
 minecraft_image = File.open(Rails.root.join("db/fixtures/games/minecraft.jpg"))
 minecraft.photo.attach(io: minecraft_image, filename: "minecraft.jpg")
+minecraft.save!
 
-counter = Game.create!(name: "Counter-Strike: Global Offensive", description: "Jeu de tir à la première personne multijoueur en ligne basé sur le jeu d'équipe.", on_steam: true, on_psn: true, on_xboxlive: true)
+counter = Game.new(name: "Counter-Strike: Global Offensive", description: "Jeu de tir à la première personne multijoueur en ligne basé sur le jeu d'équipe.", on_steam: true, on_psn: true, on_xboxlive: true)
 csgo_image = File.open(Rails.root.join("db/fixtures/games/csgo.jpg"))
 counter.photo.attach(io: csgo_image, filename: "csgo.jpg")
-
+counter.save!
 puts "Allez tiens bon ! "
 
-apex = Game.create!(name: "Apex Legends", description: "Jeu vidéo de type battle royale", on_psn: true, on_xboxlive: true)
+apex = Game.new(name: "Apex Legends", description: "Jeu vidéo de type battle royale", on_psn: true, on_xboxlive: true)
 apex_image = File.open(Rails.root.join("db/fixtures/games/apex.jpg"))
 apex.photo.attach(io: apex_image, filename: "apex.jpg")
+apex.save!
 
-teamfort = Game.create!(name: "Team Fortress 2", description: "Jeu vidéo de tir à la première personne multijoueur en ligne", on_psn: true, on_xboxlive: true)
+teamfort = Game.new(name: "Team Fortress 2", description: "Jeu vidéo de tir à la première personne multijoueur en ligne", on_psn: true, on_xboxlive: true)
 teamfortress2_image = File.open(Rails.root.join("db/fixtures/games/teamfortress2.png"))
 teamfort.photo.attach(io: teamfortress2_image, filename: "teamfortress2.png")
+teamfort.save!
 
-fifa20 = Game.create!(name: "FIFA 20", description: "Jeu vidéo de football", on_psn: true, on_xboxlive: true)
+fifa20 = Game.new(name: "FIFA 20", description: "Jeu vidéo de football", on_psn: true, on_xboxlive: true)
 fifa20_image = File.open(Rails.root.join("db/fixtures/games/fifa20.jpg"))
 fifa20.photo.attach(io: fifa20_image, filename: "fifa20.jpg")
+fifa20.save!
 puts "C'est bientôt fini "
-dofus = Game.create!(name: "Dofus", description: "Battle Royale multijoueur ou solo")
+dofus = Game.new(name: "Dofus", description: "Battle Royale multijoueur ou solo")
 dofus_image = File.open(Rails.root.join("db/fixtures/games/dofus.jpg"))
 dofus.photo.attach(io: dofus_image, filename: "dofus.jpg")
+dofus.save!
 
-lol = Game.create!(name: "League of Legends", description: "Jeu vidéo de type arène de bataille en ligne")
+lol = Game.new(name: "League of Legends", description: "Jeu vidéo de type arène de bataille en ligne")
 lol_image = File.open(Rails.root.join("db/fixtures/games/lol.jpg"))
 lol.photo.attach(io: lol_image, filename: "lol.jpg")
-
+lol.save!
 # Bob games
 UserGame.create!(user: bob, game: fortnite)
 UserGame.create!(user: bob, game: callofduty)
