faecf253df9ef4d7d1295097747820e518fcdd7c
edouard goisbault <edouard.goisbault01@gmail.com>
2020-09-03 16:06:39 +0200

    design friend btn on event creattion, add some css on differentds cards

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -2,7 +2,7 @@ import flatpickr from "flatpickr";
 
 flatpickr(".datepicker", {
   enableTime: true,
-    dateFormat: "Y-m-d H:i",
+    dateFormat: "d-m-Y H:i",
     minDate: "today",
     time_24hr: true,
     hourIncrement:1,
