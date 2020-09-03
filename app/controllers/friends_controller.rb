c84395be36fb4e9160fca2ed83657a8a29256c63
julien evano <julien.evano@audencia.com>
2020-09-03 10:56:35 +0200

    added seeds for the demo day

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -8,10 +8,17 @@ class FriendsController < ApplicationController
   def create
     @friend = Friend.new(friend_params)
     @friend.user1_id = current_user.id
-    # @friend.user2_id = @user.id
-    # @user = User.find(params[])
+
     if @friend.save
-      redirect_to users_path(anchor: "user")
+      # if the user is on a profile page, redirect to this profile page
+      if params["controller"] == "users" && params["action"] == "show"
+        redirect_to user_path(@friend.user2)
+
+      # if user is on the search, redirect to the search itself (not the index)
+      else
+        redirect_to users_path(anchor: "user")
+      end
+
     else
       render :new
     end
